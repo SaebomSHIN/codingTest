1f43d0bcb9ccade08c69dedc3509d9d32ab94c37
SaebomSHIN <vermouth0882@gmail.com>
2024-07-22 10:20:48 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,13 +1,11 @@
 def solution(price):
     if price >= 500000:
-        discount = 0.2  # 20% 할인
+        price = price * 0.8
+        
     elif price >= 300000:
-        discount = 0.1  # 10% 할인
+        price = price * 0.9
+        
     elif price >= 100000:
-        discount = 0.05  # 5% 할인
-    else:
-        discount = 0  # 할인 없음
-    
-    answer = price - (price * discount)
-    
-    return int(answer)
+        price = price * 0.95
+        
+    return int(price)
